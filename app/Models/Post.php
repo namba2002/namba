c33293408a64a4c6dbe57ee5c1676b99134e19a1
namba2002 <doragon3632@gmail.com>
2022-11-29 14:14:47 +0900

    削除完了

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+    use HasFactory;
+    
     public function getPaginateByLimit(int $limit_count = 3){
         return $this->orderBy('updated_at','DESC')->paginate($limit_count);
         
